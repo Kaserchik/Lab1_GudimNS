c76ad9f1f088952e499d98a2789559a16651c73b
Kaserchik <nikita400g@gmail.com>
2023-09-16 10:34:42 +0700

    редактирование немного чего

--- Triangle/Mathtriangle.cs
+++ Triangle/Mathtriangle.cs
@@ -11,13 +11,10 @@ namespace Triangle
     {
         private List<float> sidesList = new List<float>();
         private List<string> coordList = new List<string>();
+        private string triangleResult;
 
-        public void ReusltFind()
+        private void ResultFind()
         {
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .WriteTo.File("logs/file_.txt")
-                .CreateLogger();
 
             if ((sidesList[0] == sidesList[1]) && (sidesList[1]==sidesList[2]))
             {
@@ -26,7 +23,8 @@ namespace Triangle
                 coordList.Add("0, 0");
                 coordList.Add(sidesList[0] + ", 0");
                 coordList.Add(Convert.ToString(cos_a * sidesList[0])+Convert.ToString(sin_a*sidesList[1]));
-                Log.Information("Треугольник: равносторонний\nКоординаты A: " + coordList[0] + "\nКоординаты B: " + coordList[1] + "\nКоординаты C: " + coordList[2] + "\n__________________________________________\n");
+                triangleResult = "Треугольник: равносторонний";
+                Log.Information(triangleResult + "\n" + coordList + "\n__________________________________________\n");
             }
             else if (((sidesList[0] == sidesList[1]) && (sidesList[0] != sidesList[2])) || ((sidesList[0] == sidesList[2]) && (sidesList[0] != sidesList[1])))
             {
@@ -35,7 +33,8 @@ namespace Triangle
                 coordList.Add("0, 0");
                 coordList.Add(sidesList[0] + ", 0");
                 coordList.Add(Convert.ToString(cos_a * sidesList[0]) + Convert.ToString(sin_a * sidesList[1]));
-                Log.Information("Треугольник: равносторонний\nКоординаты A: " + coordList[0] + "\nКоординаты B: " + coordList[1] + "\nКоординаты C: " + coordList[2] + "\n__________________________________________\n");
+                triangleResult = "Треугольник: равнобедренный";
+                Log.Information(triangleResult + "\n" + coordList + "\n__________________________________________\n");
             }
             else
             {
@@ -44,7 +43,8 @@ namespace Triangle
                     coordList.Add("-1, -1");
                     coordList.Add("-1, -1");
                     coordList.Add("-1, -1");
-                    Log.Information("Треугольник: равносторонний\nКоординаты A: " + coordList[0] + "\nКоординаты B: " + coordList[1] + "\nКоординаты C: " + coordList[2] + "\n__________________________________________\n");
+                    triangleResult = "Треугольник: это не треугольник";
+                    Log.Information(triangleResult + "\n" + coordList + "\n__________________________________________\n");
                 }
                 else
                 {
@@ -53,44 +53,36 @@ namespace Triangle
                     coordList.Add("0, 0");
                     coordList.Add(sidesList[0] + ", 0");
                     coordList.Add(Convert.ToString(cos_a * sidesList[0]) + Convert.ToString(sin_a * sidesList[1]));
-                    Log.Information("Треугольник: равносторонний\nКоординаты A: " + coordList[0] + "\nКоординаты B: " + coordList[1] + "\nКоординаты C: " + coordList[2] + "\n__________________________________________\n");
+                    triangleResult = "Треугольник: равносторонний";
+                    Log.Information(triangleResult + "\n" + coordList + "\n__________________________________________\n");
                 }
             }
-            Log.CloseAndFlush();
+
         }
 
 
-        public void Start()
+        public (string,string) Start(string a, string b, string c)
         {
-            string template = "{Timestamp:HH:mm:ss} | [{Level:u3}] | {Message:lj}{NewLine}{Exception}";
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .WriteTo.Console(outputTemplate: template)
-                .WriteTo.File("logs/file_.txt", outputTemplate: template)
-                .CreateLogger();
-            while (true)
-            {
                 sidesList.Clear();
                 coordList.Clear();
                 try
                 {
-                    Console.Write("Введите длину стороны A: ");
-                    sidesList.Add(float.Parse(Console.ReadLine()));
-                    Console.Write("Введите длину стороны B: ");
-                    sidesList.Add(float.Parse(Console.ReadLine()));
-                    Console.Write("Введите длину стороны C: ");
-                    sidesList.Add(float.Parse(Console.ReadLine()));
-                    this.ReusltFind();
-                }
+                    sidesList.Add(float.Parse(a));
+                    sidesList.Add(float.Parse(b));
+                    sidesList.Add(float.Parse(c));
+                    ResultFind();
+                return (triangleResult, coordList[0]);
+            }
                 catch (Exception ex)
                 {
+                    triangleResult = "";
                     coordList.Add("-2, -2");
                     coordList.Add("-2, -2");
                     coordList.Add("-2, -2");
-                    Log.Information("Вы ввели данные не того формата.\nКоординаты A: " + coordList[0] + "\nКоординаты B: " + coordList[1] + "\nКоординаты C: " + coordList[2] + "\n");
+                    triangleResult = "";
+                Log.Information(triangleResult + "\n" + coordList + "\n__________________________________________\n");
                     Log.Error(ex, "Что-то пошло не так...");
-                    Log.CloseAndFlush();
-                }
+                return (triangleResult, coordList[0]);
             }
         }
     }
--- Triangle/Program.cs
+++ Triangle/Program.cs
@@ -12,8 +12,20 @@ namespace Triangle
     {
         static void Main()
         {
+            string template = "{Timestamp:HH:mm:ss} | [{Level:u3}] | {Message:lj}{NewLine}{Exception}";
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console(outputTemplate: template)
+                .WriteTo.File("logs/file_.txt", outputTemplate: template)
+                .CreateLogger();
+
             Mathtriangle mathtriangle = new Mathtriangle();
-            mathtriangle.Start();
+            string a = "2";
+            string b = "2";
+            string c = "26";
+            mathtriangle.Start(a, b, c);
+
+            Log.CloseAndFlush();
         }
     }
 }
